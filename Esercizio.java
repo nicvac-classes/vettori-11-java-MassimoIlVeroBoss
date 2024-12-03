6219dedb1efc72284238e128238a2b5e7cdbaa34
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2024-12-03 19:34:03 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,55 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
+import java.util.Random;
 import java.util.Scanner;
 
-// Classe principale, con metodo main
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        int n;
+        int i;
+        int j;
+        int t;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        do {
+            System.out.println("Inserire la dimensione del vettore: ");
+            n = in.nextInt();
+        } while (n <= 0);
+
+        int[] u = new int[n];
+        int[] r = new int[n];
+        int[] s = new int[n];
+        float[] m = new float[n];
+
+        for (i = 0; i < n; i++) {
+            do {
+                System.out.println("Inserire l'elemento numero " + (i + 1) + " del vettore: ");
+                u[i] = in.nextInt();
+            } while (u[i] < 0);
+        }
+
+        for (i = 0; i < n; i++) {
+            r[i] = random.nextInt(11);
+        }
+
+        for (i = 0; i < n; i++) {
+            s[i] = random.nextInt(6) + 3;
+        }
+
+        for (i = 0; i < n; i++) {
+            m[i] = (float) (u[i] + r[i] + s[i]) / 3;
+        }
+
+        System.out.println("Inserire un numero intero: ");
+        t = in.nextInt();
+
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (u[i] + u[j] == t) {
+                    System.out.println(u[i] + " + " + u[j] + " = " + t);
+                }
+            }
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
